4af9985f7b598e1cd40df59b1908a28a5052ff9e
jnyckowiak <jnyckowiak@gmail.com>
2021-02-02 00:40:24 +0100

    warmup as starting point

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Battleships
 Project
-a
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import game.Battleships;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Battleships game = new Battleships();
+        game.runGame();
     }
 }
