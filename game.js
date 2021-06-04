0a2c4c9b56c620c4444cf5ddf6abd89037cdfeec
trzciniakus <artur.trzcinski.pn@gmail.com>
2021-06-04 15:13:26 +0200

    change game.js styles.css added board-ship.png

--- game.js
+++ game.js
@@ -1,56 +1,79 @@
-
-let shipDirection = 'vertical';
+let shipDirection = "vertical";
+let gameMode = "deploy";
 let shipLength = 3;
 
-$(document).contextmenu(function(e) {
-    e.preventDefault();
-    shipDirection = (shipDirection === 'vertical' ? 'horizontal' : 'vertical');
-    $(".player-one").children().removeClass('hover');
-    $(".player-one").children().removeClass('invalid');
+//EVENT HANDLERS//
+
+$(".player-one .field").click(function (e) {
+  if (gameMode === "deploy") {
+    if ($(this).hasClass("hover")) {
+      hoveredIndex = $(this).index();
+      let incrementFactor = shipDirection === "vertical" ? 10 : 1;
+
+      for (var i = 0; i < shipLength; i++) {
+        let index = hoveredIndex + i * incrementFactor + 1;
+
+        $(".player-one .field:nth-child(" + index + ")").addClass("deployed");
+      }
+    }
+  }
 });
 
-$('.player-one .field').hover(  
-    applyClassForHover,
-    applyClassForHover
-);
+$(".player-one .field").contextmenu(function (e) {
+  e.preventDefault();
+  shipDirection = shipDirection === "vertical" ? "horizontal" : "vertical";
+  removeHoverClasses();
+  addHoverClass($(this).index());
+});
+
+$(".player-one .field").hover(applyClassForHover, removeHoverClasses);
+
+//SUPPORTING FUNCTIONS//
 
 function applyClassForHover(e) {
-    let incrementFactor = (shipDirection === 'vertical'? 10 : 1); 
-    let appliedClass = 'hover';
-    let hoveredIndex = $(this).index();
-    let endIndex = $(this).index() + (shipLength - 1) * incrementFactor ;
+  addHoverClass($(this).index());
+}
 
-    if (shipDirection === 'vertical' && endIndex >= 100){
-        appliedClass = 'invalid';
+function removeHoverClasses() {
+  $(".player-one").children().removeClass("hover");
+  $(".player-one").children().removeClass("invalid");
+}
+
+function addHoverClass(hoveredIndex) {
+  let incrementFactor = shipDirection === "vertical" ? 10 : 1;
+  let appliedClass = "hover";
+  let endIndex = hoveredIndex + (shipLength - 1) * incrementFactor;
+
+  if (shipDirection === "vertical" && endIndex >= 100) {
+    appliedClass = "invalid";
+  }
+
+  if (
+    shipDirection === "horizontal" &&
+    Math.floor(hoveredIndex / 10) != Math.floor(endIndex / 10)
+  ) {
+    appliedClass = "invalid";
+  }
+
+  for (var i = 0; i < shipLength; i++) {
+    let index = hoveredIndex + i * incrementFactor + 1;
+
+    if ($(".player-one .field:nth-child(" + index + ")").hasClass("deployed")) {
+      appliedClass = "invalid";
+      break;
+    }
+  }
+
+  for (var i = 0; i < shipLength; i++) {
+    let index = hoveredIndex + i * incrementFactor + 1;
+
+    if (
+      shipDirection === "horizontal" &&
+      Math.floor(hoveredIndex / 10) != Math.floor((index - 1) / 10)
+    ) {
+      break;
     }
 
-    if ((shipDirection === 'horizontal') && (Math.floor(hoveredIndex/10) != Math.floor(endIndex/10))){
-        appliedClass = 'invalid';
-    }
-
-    for(var i = 0; i < shipLength; i++){
-        let index = $(this).index() + i * incrementFactor + 1;
-
-        if($(".player-one .field:nth-child(" + index + ")").hasClass("deployed")){
-            appliedClass = 'invalid';
-            break;
-        }
-
-    }
-
-
-    if(e.type === 'mouseenter') {
-        for(var i = 0; i < shipLength; i++){
-            let index = $(this).index() + i * incrementFactor + 1;
-
-            if ((shipDirection === 'horizontal') && (Math.floor(hoveredIndex/10) != Math.floor((index - 1)/10))){
-                break;
-            }
-
-            $(".player-one .field:nth-child(" + index + ")").addClass(appliedClass);
-        }
-    } else {
-        $(".player-one").children().removeClass(appliedClass);
-    }
-
+    $(".player-one .field:nth-child(" + index + ")").addClass(appliedClass);
+  }
 }
